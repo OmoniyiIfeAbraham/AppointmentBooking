b52eb4d82c74cb14f26516795a050bf0351e21dd
OmoniyiIfeAbraham <ife04abraham@gmail.com>
2022-11-17 12:41:59 +0100

    Update

--- router/remark/remark.js
+++ router/remark/remark.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const patients = require('./../../models/patient/profile/profile')
+const patientMod = require('./../../models/patient/profile/profile')
 const doctors = require('./../../models/doctor/profile/profile')
 const remarkMod = require('./../../models/remarks/remarks')
+const scheduleMod = require('./../../models/doctor/schedule/schedule')
+const bookingMod = require('./../../models/patient/bookings/bookings')
 
 router.get('/', (req, res) => {
     console.log('Iya ooooo')
@@ -28,11 +30,12 @@ router.post('/p/:sender/:receiver', async(req, res, next) => {
                 await remark.save()
                 res.redirect(`/patient#remark/${sender}/${receiver}`)
             } else {
-
+                res.redirect(`/patient#remark/${sender}/${receiver}`)
             }
         } catch (err) {
             console.log(err)
             next(err)
+            res.redirect(`/patient#remark/${sender}/${receiver}`)
         }
     } else {
         res.redirect('/patientLogin')
@@ -49,6 +52,10 @@ router.post('/d/:sender/:receiver/:id', async(req, res, next) => {
         const message = req.body.message
         const sender = req.params.sender
         const receiver = req.params.receiver
+        const schedule = await scheduleMod.findById({ _id: req.params.id})
+        const bookings = await bookingMod.find({ scheduleID: req.params.id }).sort({ createdAt: -1 })
+        const patients = await patientMod.find()
+        const remarks = await remarkMod.find()
         try {
             if (message != null) {
                 const remark = new remarkMod({
@@ -59,11 +66,12 @@ router.post('/d/:sender/:receiver/:id', async(req, res, next) => {
                 await remark.save()
                 res.redirect(`/viewSchedule/${req.params.id}#remark/${sender}/${receiver}`)
             } else {
-
+                res.render('doctor/schedules/viewSchedule', { schedule, bookings, patients, remarks, msg: 'Message cannot be Empty' })
             }
         } catch (err) {
             console.log(err)
             next(err)
+            res.render('doctor/schedules/viewSchedule', { schedule, bookings, patients, remarks, msg: `${err.message}` })
         }
     } else {
         res.redirect('/doctorLogin')
