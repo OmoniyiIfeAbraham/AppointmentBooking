38e180b54f4aecad667570d3064f3789463b5665
OmoniyiIfeAbraham <ife04abraham@gmail.com>
2022-11-25 03:05:22 +0100

    Update

--- router/doctor/patients/patients.js
+++ router/doctor/patients/patients.js
@@ -2,13 +2,18 @@ const express = require('express')
 const router = express.Router()
 
 const profileMod = require('./../../../models/patient/profile/profile')
+const registerMod = require('./../../../models/doctor/auth/register')
+const doctorMod = require('./../../../models/doctor/profile/profile')
 
 router.get('/', async(req, res) => {
     const sess = req.session
     if ( sess.email && sess.password && sess.identifier === 'doctor') {
         const patients = await profileMod.find()
+        const profile = await registerMod.findOne({ email: sess.email })
+        const person = await doctorMod.findOne({ uniqueID: profile._id })
+        const you = await doctorMod.findOne({ email: sess.email })
         console.log(patients)
-        res.render('doctor/patients/patients', { patients, msg: '' })
+        res.render('doctor/patients/patients', { patients, msg: '', unique: profile._id, you, id: person._id })
     } else {
         res.redirect('/doctorLogin')
     }
